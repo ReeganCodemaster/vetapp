1ab860be4d3065c716e1338ed99da68a38818a7e
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-07 17:01:10 +0200

    added ControllerSpecHelper and removed type: :request from RequestSpecHelper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,7 +49,8 @@ RSpec.configure do |config|
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
