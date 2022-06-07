04ddd7d4b16f8fecd8d325644cef0717e52abf01
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-07 14:57:05 +0200

    added email feild to users

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_120702) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
     t.string "password"
     t.string "password_digest"
     t.datetime "created_at", null: false
