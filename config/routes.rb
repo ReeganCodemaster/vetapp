55be7e13dabe6cdecac5890aff3de3b7fbf27652
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-08 13:08:55 +0200

    fixed syntax error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  routes 'auth/login;', to: 'authentication#authenticate'
+  routes 'auth/login', to: 'authentication#authenticate'
   # Defines the root path route ("/")
   # root "articles#index"
 end
