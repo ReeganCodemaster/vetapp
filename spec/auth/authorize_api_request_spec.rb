1bccb2be0c63e8c504352c1ca114a93afaad2c33
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-08 09:37:56 +0200

    added tests for invalid requests

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -16,8 +16,36 @@ RSpec.describe AuthorizeApiRequest do
 
     context 'when request is invalid' do
       context 'when missing token' do
-        it 'raises MissingToken error' do
+        it 'raises a MissingToken error' do
           expect {invalid_request.call}.to raise_error(ExceptionHandler::MissingToken, 'Missing token') 
+        end
+      end
+
+      context 'when token is invalid' do
+        subject(:invalid_request) do
+          described_class.new('Authorization' => token_generator(5))
+        end
+
+        it 'raises an InvalidToken error' do
+          expect {invalid_request.call}.to raise_error(ExceptionHandler::InvalidToken, 'Invalid token')
+        end
+      end
+
+      context 'when token is expired' do
+        let(:header) { {'Authorization' => invalid_token_genertor(user.id)} }
+        subject(:invalid_request) {described_class.new(:header)}
+
+        it 'raises ExceptionHandler::ExpiredSignature error' do
+          expect {invalid_request.call}.to raise_error(ExceptionHandler::ExpiredSignature, 'Signatrure has expired')
+        end
+      end
+
+      context 'when token is fake' do
+        let(:header) { {'Authorization' => 'foobar'} }
+        subject(:invalid_request) {described_class.new(:header)}
+
+        it 'handles JWT::DecodeError' do
+          expect {invalid_request.call}.to raise_error(ExceptionHandler::InvalidToken, 'Not enough or too many segments')
         end
       end
     end
