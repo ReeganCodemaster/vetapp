0af23c611042b06445df3f228a1547450cd21acf
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-08 09:53:28 +0200

    added methods to class

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -1,3 +1,22 @@
 class AuthorizeApiRequest
-  
+  def initialize(headers = {})
+    @headers = headers
+  end
+
+  def call
+    {
+      user: user
+    }
+  end
+
+  private
+  def user
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+  rescue ActiveRecord::RecordNotFound => error
+    raise(ExceptionHandler::InvalidToken, ("#{Message.invalid_token} {error.message}"))
+  end
+
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+  end
 end
