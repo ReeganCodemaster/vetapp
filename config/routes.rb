01f33714e44d8104e9b4a568a2ec357cc2755d41
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-08 17:15:25 +0200

    added signup to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post 'auth/login', to: 'authentication#authenticate'
+  post '/signup', to: 'users#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
