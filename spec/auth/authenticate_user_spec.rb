70ec1692ca256d410cd03fec2b39c20e7bd852c2
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-08 11:31:26 +0200

    added test for valid credentails

--- spec/auth/authenticate_user_spec.rb
+++ spec/auth/authenticate_user_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe AuthenticateUser do 
   let(:user) { create(:user) }
-  subject(:valid_auth_obj) {described_class.new(user.email, user.password)}
-  subject(:invalid_auth_obj) {described_class.new('foo','bar')}
+  subject(:valid_auth) {described_class.new(user.email, user.password)}
+  subject(:invalid_auth) {described_class.new('foo','bar')}
+
+  describe '#call' do
+    context 'when credentials are valid' do
+      it 'returns a valid auth token' do
+        token = valid_auth.call
+        expect(token).not_to be_nil
+      end
+    end
+  end
 end
