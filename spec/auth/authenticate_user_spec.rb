ad58ba3cb6e600b459f0e512b3f4c99141bd51eb
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-08 11:10:58 +0200

    set up test data

---
+++ spec/auth/authenticate_user_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe AuthenticateUser do 
+  let(:user) { create(:user) }
+  subject(:valid_auth_obj) {described_class.new(user.email, user.password)}
+  subject(:invalid_auth_obj) {described_class.new('foo','bar')}
+end
