f2dde735633a180b8ecea905f3567399d9fc07b6
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-08 13:34:39 +0200

    set up test data

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Authentications", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let(:headers) { valid_heaaders.exept('Authorization')}
+  let(:valid_credentials) do
+    {
+    email: user.email,
+    password:user.password
+    }.to_json
+  let(:invalid_credentials) do
+    {
+      email: Faker::Internet.email,
+      password: Faker::Internet.password
+    }.to_json
+
   end
 end
