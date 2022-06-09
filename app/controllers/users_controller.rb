3a750ea238c6a50c66cd15069b7c0cb7ec7fba82
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-09 15:05:44 +0200

    added authentication for role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   def create
     user = User.create!(user_params)
-    auth_token = AuthenticateUser.new(user.email, user.password).call
+    auth_token = AuthenticateUser.new(user.email, user.password, user.role).call
     response = { message:  Message.account_created, auth_token: auth_token }
     json_response(response, :created)
   end
 
   private
   def user_params 
-    params.permit(:name,:email,:password,:password_confirmation)
+    params.permit(:name,:email,:password,:password_confirmation, :role)
   end
 end
