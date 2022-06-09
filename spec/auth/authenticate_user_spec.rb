d17eb874296cc7cbae27283665e482925d769fd3
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-09 09:50:50 +0200

    added context for when role is owner

--- spec/auth/authenticate_user_spec.rb
+++ spec/auth/authenticate_user_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe AuthenticateUser do 
-  let(:user) { create(:user) }
-  subject(:valid_auth) {described_class.new(user.email, user.password)}
-  subject(:invalid_auth) {described_class.new('foo','bar')}
-
   describe '#call' do
-    context 'when credentials are valid' do
-      it 'returns a valid auth token' do
-        token = valid_auth.call
-        expect(token).not_to be_nil
-      end
-    end
+    context 'when role is owner' do
+      let(:user) { create(:user, role: 'owner') }
+      subject(:valid_auth) {described_class.new(user.email, user.password)}
+      subject(:invalid_auth) {described_class.new('foo','bar')}
 
-    context 'when credentials are invalid' do
-      it 'raises an authentication error' do
-        expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
+      context 'when credentials are valid' do
+        it 'returns a valid auth token' do
+          token = valid_auth.call
+          expect(token).not_to be_nil
+        end
+      end
+      
+      context 'when credentials are invalid' do
+        it 'raises an authentication error' do
+          expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
+        end
       end
     end
   end
