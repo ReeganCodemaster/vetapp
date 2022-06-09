1119b61a49592d4d7464a7b586e62d383dedfce2
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-09 10:22:33 +0200

    added tests according to roles

--- spec/auth/authenticate_user_spec.rb
+++ spec/auth/authenticate_user_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe AuthenticateUser do
     context 'when role is owner' do
       let(:user) { create(:user, role: 'owner') }
       subject(:valid_auth) {described_class.new(user.email, user.password, user.role)}
-      subject(:invalid_auth) {described_class.new('foo','bar')}
 
       context 'when credentials are valid' do
         it 'returns a valid auth token' do
@@ -15,7 +14,20 @@ RSpec.describe AuthenticateUser do
       end
 
       context 'when credentials are invalid' do
-        context 'when submitted role is vet' do
+        context 'when user does not exist' do
+          subject(:invalid_auth) {described_class.new('foo', user.password, user.role)}
+          it 'raises an authentication error' do
+            expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
+          end
+        end
+        context 'when the passsword is incorrect' do
+          subject(:invalid_auth) {described_class.new(user.email, ' ', user.role)}
+          it 'raises an authentication error' do
+            expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
+          end
+        end
+        
+        context 'when wrong role is submitted' do
           let(:user) { create(:user, role: 'vet') }
           subject(:invalid_auth) {described_class.new(user.email, user.password, user.role)}
 
@@ -23,18 +35,13 @@ RSpec.describe AuthenticateUser do
             expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
           end
         end
-
-        it 'raises an authentication error' do
-          expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
-        end
       end
     end
 
     context 'when role is vet' do
       let(:user) { create(:user, role: 'vet') }
       subject(:valid_auth) {described_class.new(user.email, user.password, user.role)}
-      subject(:invalid_auth) {described_class.new('foo','bar')}
-      
+
       context 'when credentials are valid' do
         it 'returns a valid auth token' do
           token = valid_auth.call
@@ -43,17 +50,26 @@ RSpec.describe AuthenticateUser do
       end
 
       context 'when credentials are invalid' do
-        context 'when submitted role is owner' do
+        context 'when user does not exist' do
+          subject(:invalid_auth) {described_class.new('foo', user.password, user.role)}
+          it 'raises an authentication error' do
+            expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
+          end
+        end
+        context 'when the passsword is incorrect' do
+          subject(:invalid_auth) {described_class.new(user.email, ' ', user.role)}
+          it 'raises an authentication error' do
+            expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
+          end
+        end
+        
+        context 'when wrong role is submitted' do
           let(:user) { create(:user, role: 'owner') }
           subject(:invalid_auth) {described_class.new(user.email, user.password, user.role)}
 
           it 'raises an authentication error' do
             expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
           end
-        end
-
-        it 'raises an authentication error' do
-          expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
         end
       end
     end
