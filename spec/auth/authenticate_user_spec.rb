58aefb68d71a39684345018836727c1d4d0b7640
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-09 10:00:19 +0200

    added tests for roles

--- spec/auth/authenticate_user_spec.rb
+++ spec/auth/authenticate_user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe AuthenticateUser do
   describe '#call' do
     context 'when role is owner' do
       let(:user) { create(:user, role: 'owner') }
-      subject(:valid_auth) {described_class.new(user.email, user.password)}
+      subject(:valid_auth) {described_class.new(user.email, user.password, user.role)}
       subject(:invalid_auth) {described_class.new('foo','bar')}
 
       context 'when credentials are valid' do
@@ -13,8 +13,45 @@ RSpec.describe AuthenticateUser do
           expect(token).not_to be_nil
         end
       end
-      
+
       context 'when credentials are invalid' do
+        context 'when submitted role is vet' do
+          let(:user) { create(:user, role: 'vet') }
+          subject(:invalid_auth) {described_class.new(user.email, user.password, user.role)}
+
+          it 'raises an authentication error' do
+            expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
+          end
+        end
+
+        it 'raises an authentication error' do
+          expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
+        end
+      end
+    end
+
+    context 'when role is vet' do
+      let(:user) { create(:user, role: 'vet') }
+      subject(:valid_auth) {described_class.new(user.email, user.password, user.role)}
+      subject(:invalid_auth) {described_class.new('foo','bar')}
+      
+      context 'when credentials are valid' do
+        it 'returns a valid auth token' do
+          token = valid_auth.call
+          expect(token).not_to be_nil
+        end
+      end
+
+      context 'when credentials are invalid' do
+        context 'when submitted role is owner' do
+          let(:user) { create(:user, role: 'owner') }
+          subject(:invalid_auth) {described_class.new(user.email, user.password, user.role)}
+
+          it 'raises an authentication error' do
+            expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
+          end
+        end
+
         it 'raises an authentication error' do
           expect {invalid_auth.call}.to raise_error(ExceptionHandler::AuthenticationError, /Invalid credentials/)
         end
