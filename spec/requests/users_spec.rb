ec2d8e713591b04d83cd1d5fc87edf75bf525a25
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-09 15:05:53 +0200

    added tests for roles

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,37 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe 'Users API' do
-  let(:user) {build(:user)}
-  let(:headers) { valid_headers.except('Authorization') }
-  let(:valid_attributes) { attributes_for(:user, password_confirmation: user.password) }
-
   describe 'POST /signup' do
-    context 'when the request is valid' do
-      before { post '/signup', params:valid_attributes.to_json, headers: headers}
-      
-      it 'creates a new user' do
-        expect(response).to have_http_status(201)
-      end
+    context 'role is owner' do
+      let(:user) {create(:user, role:'owner')}
+      let(:headers) { valid_headers.except('Authorization') }
+      let(:valid_attributes) { attributes_for(:user, password_confirmation: user.password, role: user.role) }
+      context 'when the request is valid' do
+        before { post '/signup', params:valid_attributes.to_json, headers: headers}
+        
+        it 'creates a new user' do
+          expect(response).to have_http_status(201)
+        end
 
-      it 'returns a success mesage' do
-        expect(json['message']).to  match(/Account created succesfully/)
-      end
+        it 'returns a success mesage' do
+          expect(json['message']).to  match(/Account created succesfully/)
+        end
 
-      it 'returns a token' do
-        expect(json['auth_token']).not_to be_nil
+        it 'returns a token' do
+          expect(json['auth_token']).not_to be_nil
+        end
       end
     end
-  end
 
-  context 'when the request is invalid' do
-    before { post '/signup', params:{}, headers: headers }
+    context 'when the request is invalid' do
+      before { post '/signup', params:{}, headers: headers }
 
-    it 'does not create a new user' do
-      expect(response).to have_http_status(422)
+      it 'does not create a new user' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a failure message' do
+        expect(json['message']).to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
+      end
     end
 
-    it 'returns a failure message' do
-      expect(json['message']).to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
+    context 'role is vet' do
+      let(:user) {create(:user, role:'vet')}
+      let(:headers) { valid_headers.except('Authorization') }
+      let(:valid_attributes) { attributes_for(:user, password_confirmation: user.password, role: user.role) }
+      context 'when the request is valid' do
+        before { post '/signup', params:valid_attributes.to_json, headers: headers}
+        
+        it 'creates a new user' do
+          expect(response).to have_http_status(201)
+        end
+
+        it 'returns a success mesage' do
+          expect(json['message']).to  match(/Account created succesfully/)
+        end
+
+        it 'returns a token' do
+          expect(json['auth_token']).not_to be_nil
+        end
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/signup', params:{}, headers: headers }
+
+      it 'does not create a new user' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a failure message' do
+        expect(json['message']).to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
+      end
     end
   end
 end
