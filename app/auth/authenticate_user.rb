3b5b5032d70dd597649d089978d6b9470f1f6b24
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-10 10:38:56 +0200

    added methods for signup and signin

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -1,20 +1,32 @@
 class AuthenticateUser
-  def initialize(email, password, role='none')
+  def initialize(email, password, role)
     @email = email
     @password = password
     @role = role
   end
 
-  def call
-    JsonWebToken.encode(user_id: user.id) if user
+  def signup
+    JsonWebToken.encode(user_id: user('signup').id) if user('signup')
+  end
+
+  def signin
+    JsonWebToken.encode(user_id: user('signin').id) if user('signin')
   end
 
   private
-  def user
-    user = User.find_by(email: @email)
-    if user && user.authenticate(@password) && (user.role == @role || user.role == 'none')
-      return user 
+  def user(action)
+    if action == 'signup'
+      user = User.find_by(email: @email)
+      if user && user.authenticate(@password) && user.role == @role
+        return user 
+      end
+      raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
+    elsif action == 'signin'
+      user = User.find_by(email: @email)
+      if user && user.authenticate(@password)
+        return user 
+      end
+      raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
     end
-    raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
   end
 end
