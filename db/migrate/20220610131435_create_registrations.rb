8e4aaad9bd0935454907facee9c4603187befffb
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-10 15:27:33 +0200

    aded registrations tabel

---
+++ db/migrate/20220610131435_create_registrations.rb
@@ -0,0 +1,11 @@
+class CreateRegistrations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :registrations do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :pet, null: false, foreign_key: true
+      t.datetime :registration_date
+
+      t.timestamps
+    end
+  end
+end
