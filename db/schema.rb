b47b04d66ec542c839738dc43ae51bc99ca6112f
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-10 15:27:24 +0200

    aded registrations tabel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_10_115238) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_10_131435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_10_115238) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "registrations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "pet_id", null: false
+    t.datetime "registration_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pet_id"], name: "index_registrations_on_pet_id"
+    t.index ["user_id"], name: "index_registrations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -39,4 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_10_115238) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "registrations", "pets"
+  add_foreign_key "registrations", "users"
 end
