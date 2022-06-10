bd914bc45aa94a0632e7ecf7e5005865d6abfc58
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-10 13:30:54 +0200

    sskip authentication on sigup and login routes

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,4 +1,5 @@
 class AuthenticationController < ApplicationController
+  skip_before_action :authorize_request, only: :authenticate
   def authenticate
     auth_response = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call('signin')
     json_response(auth_token: auth_response[:auth_token], user: auth_response[:user])
