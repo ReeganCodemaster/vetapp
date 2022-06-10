3f99336278fe1a4a7adda75e3ed0eca3590c8194
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-10 14:58:52 +0200

    created m2m realtionship with users through appointments

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,17 @@
 class PetsController < ApplicationController
+  def create
+    pet = current_user.pets.create!(pet_params)
+    json_response(message:Message.pet_created)
+  end
+
+  def register
+    pet = Pet.find(pet_params[:id])
+    vet = User.find_by(email: pet_params[:vet_email])
+    pet.PetsUsers.user_id = vet.id
+  end
+
+  private
+  def pet_params
+    params.permit(:id, :name, :animal, :vet_email)
+  end
 end
