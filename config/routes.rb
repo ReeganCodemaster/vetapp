6cac5b4ff8c23b4c5bd7a2a062faa2c04d1536c3
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-10 15:26:30 +0200

    added create a and registration route for animal

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post 'auth/login', to: 'authentication#authenticate'
   post '/signup', to: 'users#create'
+  post '/pets/create', to: 'pets#create'
+  post 'pets/:id/register', to:'pets#register'
   # Defines the root path route ("/")
   # root "articles#index"
 end
