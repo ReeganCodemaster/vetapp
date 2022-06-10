fbe75b9662382e31b864bc2db86abdff1cb662d9
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-10 10:46:12 +0200

    changed signup to call

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,6 @@
 class AuthenticationController < ApplicationController
   def authenticate
-    auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call('signup')
     json_response(auth_token: auth_token)
   end
 
