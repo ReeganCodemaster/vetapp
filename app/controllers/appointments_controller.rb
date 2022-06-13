38522ee1a6161a37e744bbb7ceb21bfa437e86c3
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-13 14:24:24 +0200

    fixed appointments association

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,11 @@
 class AppointmentsController < ApplicationController
+  def create
+    appointment = Appointment.create!(appointment_pararms)
+    json_response(message:Message.apointment_succesfull, user:appointment.user)
+  end
+
+  private
+  def appointment_pararms
+    params.permit(:pet_id, :id, :appointment_date)
+  end
 end
