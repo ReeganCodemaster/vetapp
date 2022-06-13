02c782db12c61d609cda8669cb00c49d9a50cfab
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-13 10:29:30 +0200

    removed vets controller

--- app/controllers/vets_controller.rb
+++
@@ -1,6 +0,0 @@
-class VetsController < ApplicationController
-  def registrations
-    registrations = Registration.where(user_id: params[:id]).where(accepted: false)
-    json_response(registrations)
-  end
-end
