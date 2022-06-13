8703a5f4923e59de47e3ef8edd1009251c87dd5e
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-13 14:15:30 +0200

    merged migrations into single migration

--- db/migrate/20220610115238_create_appointments.rb
+++
@@ -1,9 +0,0 @@
-class CreateAppointments < ActiveRecord::Migration[7.0]
-  def change
-    create_table :appointments do |t|
-      t.belongs_to :users
-      t.belongs_to :pets
-      t.datetime :appointment_date
-    end
-  end
-end
--- db/migrate/20220610115238_create_appointments_and_registrations.rb
+++ db/migrate/20220610115238_create_appointments_and_registrations.rb
@@ -1,4 +1,4 @@
-class CreateRegistrations < ActiveRecord::Migration[7.0]
+class CreateAppointmentsAndRegistrations < ActiveRecord::Migration[7.0]
   def change
     create_table :registrations do |t|
       t.references :user, null: false, foreign_key: true
@@ -6,6 +6,12 @@ class CreateRegistrations < ActiveRecord::Migration[7.0]
       t.datetime :registration_date
       t.boolean :accepted, default: false
       t.timestamps
+    end
+    
+    create_table :appointments do |t|
+      t.belongs_to :users
+      t.belongs_to :registrations
+      t.datetime :appointment_date
     end
   end
 end
