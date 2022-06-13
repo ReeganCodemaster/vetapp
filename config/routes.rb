8f6481b05c3780b5336d16323395d406017fb354
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-13 10:03:29 +0200

    added route to get all false registrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   post '/pets/create', to: 'pets#create'
   post 'pets/:id/register', to:'pets#register'
+  get 'vets/:id/registrations', to: 'vets#registrations'
   # Defines the root path route ("/")
   # root "articles#index"
 end
