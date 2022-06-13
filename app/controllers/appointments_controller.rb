b948baaefcdf0a84609b84ea1cfa56242dc7600d
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-13 17:20:19 +0200

    refactored controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,11 +1,14 @@
 class AppointmentsController < ApplicationController
   def create
-    appointment = Appointment.create!(appointment_pararms)
-    json_response(message:Message.apointment_succesfull, user:appointment.user)
+    pet = Pet.find(appointment_params[:id])
+    vet = User.find_by(email: appointment_params[:vet_email])
+    registration = pet.registrations.find_by(user_id: vet.id)
+    appointment = registration.appointments.create!(user_id: vet.id, date: appointment_params[:date].to_datetime)
+    json_response(message: Message.appointment_succesfull, user:appointment.user)
   end
 
   private
-  def appointment_pararms
-    params.permit(:pet_id, :id, :appointment_date)
+  def appointment_params
+    params.permit(:id, :vet_email, :date)
   end
 end
