a6cf72351003bfa78a1a19a5aa95e7b4e0b2a724
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-14 16:05:25 +0200

    added password to user

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,6 +2,7 @@ class AuthenticationController < ApplicationController
   skip_before_action :authorize_request, only: :authenticate
   def authenticate
     user = User.find_by(email: auth_params[:email])
+    user.password = auth_params[:password]
     auth_response(user)
   end
 
