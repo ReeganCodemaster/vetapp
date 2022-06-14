0210b52ab2b98102b82573952ee3013031773087
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-14 15:15:57 +0200

    grouped routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post 'auth/login', to: 'authentication#authenticate'
   post '/signup', to: 'users#create'
+
   post '/pets/create', to: 'pets#create'
   post 'pets/:id/register', to:'pets#register'
+  post 'pets/:id/appointment', to:'appointments#create'
 
   get 'vets/:id/registrations', to: 'users#registrations'
+  
   post 'registrations/:id', to: 'registrations#accept_registration'
-  post 'pets/:id/appointment', to:'appointments#create'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
