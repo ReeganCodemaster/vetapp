f7c5a241783689f9eb0524d4fe08af50131493ca
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-14 15:24:30 +0200

    added owner method

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,8 @@ class Pet < ApplicationRecord
   has_many :registrations
   has_many :users, through: :registrations
   # has_many :appointments, through: :registrations
+
+  def owner
+    self.users.where(role: 'owner')
+  end
 end
