ed7f35852f712a402b020e5a6a23945a08a7728a
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-14 15:16:16 +0200

    removed action from calsses

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -1,33 +1,19 @@
 class AuthenticateUser
-  def initialize(email, password, role='')
+  def initialize(email, password)
     @email = email
     @password = password
-    @role = role
   end
 
-  def call(action)
-    user_info = user(action)
-    auth_token = JsonWebToken.encode(user_id: user_info.id) if user_info
-    {
-      auth_token: auth_token,
-      user: user_info
-    }
+  def call()
+    JsonWebToken.encode(user_id: user.id) if user
   end
 
   private
-  def user(action)
-    if action == 'signup'
-      user = User.find_by(email: @email)
-      if user && user.authenticate(@password) && user.role == @role
-        return user 
-      end
-      raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
-    elsif action == 'signin'
-      user = User.find_by(email: @email)
-      if user && user.authenticate(@password)
-        return user 
-      end
-      raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
+  def user
+    user = User.find_by(email: @email)
+    if user && user.authenticate(@password)
+      return user 
     end
+    raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
   end
 end
