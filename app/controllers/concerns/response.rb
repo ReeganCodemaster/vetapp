193a6cf90aafc30d2150122169cdf92714ccc33e
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-14 15:20:03 +0200

    added auth response method

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -2,4 +2,8 @@ module Response
   def json_response(object, status= :ok)
     render json: object, status: status
   end
+
+  def auth_response(object, status= :ok)
+    render json: object, serializer: AuthUserSerializer, status: status
+  end
 end
