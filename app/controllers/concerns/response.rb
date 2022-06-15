5afb70a7cb642cb2b788ec975534f3e4b3fdfeba
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-15 11:43:08 +0200

    adde d recursivbe nesting for serializers

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,9 +1,9 @@
 module Response
   def json_response(object, status= :ok)
-    render json: object, status: status
+    render json: object, status: status, include: '**'
   end
 
   def auth_response(object, status= :ok)
-    render json: object, serializer: AuthUserSerializer, status: status
+    render json: object, serializer: AuthUserSerializer, status: status, include: '**'
   end
 end
