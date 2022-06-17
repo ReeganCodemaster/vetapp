8f15e227b18cde03772ca18456562a21645885f7
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-17 11:49:11 +0200

    added pet to registration

--- app/serializers/registration_serializer.rb
+++ app/serializers/registration_serializer.rb
@@ -1,5 +1,9 @@
 class RegistrationSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :pet_id, :registration_date, :accepted
+  attributes :id, :user_id, :pet_id, :registration_date, :accepted, :pet
+
+  def pet
+    object.pet
+  end
 
   has_many :appointments
 end
