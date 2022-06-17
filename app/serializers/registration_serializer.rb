2a888f382b61f96be14839bbcaba01d57e8857a8
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-17 13:48:41 +0200

    removed pet association

--- app/serializers/registration_serializer.rb
+++ app/serializers/registration_serializer.rb
@@ -1,9 +1,5 @@
 class RegistrationSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :pet_id, :registration_date, :accepted, :pet
-
-  def pet
-    object.pet
-  end
+  attributes :id, :user_id, :pet_id, :registration_date, :accepted
 
   has_many :appointments
 end
