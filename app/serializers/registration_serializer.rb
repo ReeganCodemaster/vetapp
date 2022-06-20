05d8ee12912f4e30bc198c60e9e89b69bbf6307e
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-20 11:54:05 +0200

    added user attribute

--- app/serializers/registration_serializer.rb
+++ app/serializers/registration_serializer.rb
@@ -1,5 +1,7 @@
 class RegistrationSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :pet_id, :registration_date, :accepted
-
+  attributes :id, :user_id, :pet_id, :registration_date, :accepted, :user
+   def user 
+    object.user.name
+   end
   has_many :appointments
 end
