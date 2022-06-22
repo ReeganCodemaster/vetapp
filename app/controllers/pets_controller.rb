1a7e804267ea506c6b02a75a558e6a33bbf82df0
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-22 09:14:41 +0200

    added cahe writes on asociation updates

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,6 +7,10 @@ class PetsController < ApplicationController
   def register
     pet = Pet.find(pet_params[:id])
     vet = User.find_by(email: pet_params[:vet_email])
+
+    user = pet.users.where(role: 'owner').last
+    Rails.cache.write("user/#{user.id}",user, expires_in: 12.hours,) 
+
     pet.registrations.create!(user_id:vet.id, registration_date: DateTime.now, vet_registration: true)
     json_response(current_user)
   end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,9 @@ class RegistrationsController < ApplicationController
     registration = Registration.find(registration_params[:id])
     registration.update(accepted: true)
 
+    user = registration.pet.users.where(role: 'owner').last
+    Rails.cache.write("user/#{user.id}", user, expires_in: 12.hours) 
+
     json_response(registration.user)
   end
 
