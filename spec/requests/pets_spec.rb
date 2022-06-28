0f2aa038aac5a2bb4a26de30b424e483facbac46
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-28 13:57:44 +0200

    added tests for invalid requests

--- spec/requests/pets_spec.rb
+++ spec/requests/pets_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Pets', type: :request do
+  let(:user) { create(:user, role: 'owner')}
+  let(:pet) { build(:pet) }
+  let(:headers) {{'Authorization' => token_generator(user.id)}}
+  
+  
   describe '/pets/create' do
-    let(:user) { create(:user, role: 'owner')}
-    let(:pet) {build(:pet)}
-    let(:headers) {{'Authorization' => token_generator(user.id)}}
-    let(:valid_attributes) { attributes_for(:pet, name: pet.name, animal: pet.animal ) }
-    
-
     context 'when the request is valid ' do
+
+      let(:valid_attributes) { attributes_for(:pet, name: pet.name, animal: pet.animal ) }
 
       before { post '/pets/create', params:valid_attributes, headers: headers}
 
@@ -32,6 +33,39 @@ RSpec.describe 'Pets', type: :request do
 
       it 'retruns a failure message' do
         expect(json['message']).to match(/Validation failed: Name can't be blank, Animal can't be blank/)
+      end
+    end
+  end
+
+  describe 'pets/:id/register' do
+    let(:vet) { create(:user, role: 'vet')}
+    let(:pet) { user.pets.create!(
+      name: Faker::Name.name ,
+      animal: Faker::Creature::Animal.name
+    )}
+    context 'when the request is valid' do
+      let(:valid_attributes) { attributes_for(:pet, vet_email: vet.email) }
+
+      before { post "/pets/#{pet.id}/register", params: valid_attributes, headers: headers }
+
+      it ' creates a new registration' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns the user' do 
+        expect(json['id']).to eq(user.id)
+      end
+    end
+
+    context 'when the  request is invalid' do
+      before { post "/pets/#{pet.id}/register", params: {}, headers: headers }
+
+      it 'does not create a pet' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'retruns an error message' do
+        expect(json['message']).to match(/Vet does't exist/)
       end
     end
   end
