c04ce6bf4ec03874a5059a65abbad93f0a378c79
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-28 11:04:51 +0200

    fixed tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,24 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Users API' do
   describe 'POST /signup' do
-    context 'role is owner' do
-      let(:user) {create(:user, role:'owner')}
-      let(:headers) { valid_headers.except('Authorization') }
-      let(:valid_attributes) { attributes_for(:user, password_confirmation: user.password, role: user.role) }
-      context 'when the request is valid' do
-        before { post '/signup', params:valid_attributes.to_json, headers: headers}
-        
-        it 'creates a new user' do
-          expect(response).to have_http_status(201)
-        end
 
-        it 'returns a success mesage' do
-          expect(json['message']).to  match(/Account created succesfully/)
-        end
+    let(:user) {build(:user, role:'owner')}
+    let(:headers) { valid_headers.except('Authorization') }
+    let(:valid_attributes) { attributes_for(:user, password_confirmation: user.password, role: user.role) }
+    context 'when the request is valid' do
 
-        it 'returns a token' do
-          expect(json['auth_token']).not_to be_nil
-        end
+      before { post '/signup', params:valid_attributes.to_json, headers: headers}
+      
+      it 'creates a new user' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns a token' do
+        expect(json['auth_token']).not_to be_nil
       end
     end
 
@@ -33,39 +29,6 @@ RSpec.describe 'Users API' do
       it 'returns a failure message' do
         expect(json['message']).to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
       end
-    end
-
-    context 'role is vet' do
-      let(:user) {create(:user, role:'vet')}
-      let(:headers) { valid_headers.except('Authorization') }
-      let(:valid_attributes) { attributes_for(:user, password_confirmation: user.password, role: user.role) }
-      context 'when the request is valid' do
-        before { post '/signup', params:valid_attributes.to_json, headers: headers}
-        
-        it 'creates a new user' do
-          expect(response).to have_http_status(201)
-        end
-
-        it 'returns a success mesage' do
-          expect(json['message']).to  match(/Account created succesfully/)
-        end
-
-        it 'returns a token' do
-          expect(json['auth_token']).not_to be_nil
-        end
-      end
-    end
-
-    context 'when the request is invalid' do
-      before { post '/signup', params:{}, headers: headers }
-
-      it 'does not create a new user' do
-        expect(response).to have_http_status(422)
-      end
-
-      it 'returns a failure message' do
-        expect(json['message']).to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
-      end
-    end
+    end 
   end
 end
