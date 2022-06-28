367e5517918b8deefdda2964f9802bfe7620b9d2
ReeganCodemaster <reeganviljoen@outlook.com>
2022-06-28 11:04:47 +0200

    fixed tests

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,72 +2,34 @@ require 'rails_helper'
 
 RSpec.describe "Authentication", type: :request do
   describe 'Post /auth/login' do
-    context 'when role is owner' do
-      let!(:user) { create(:user, role:'owner') }
-      let(:headers) { valid_headers.except('Authorization')}
-      let(:valid_credentials) do
-        {
-          email: user.email,
-          password: user.password,
-          role: user.role
-        }.to_json
-      end
-      let(:invalid_credentials) do
-        {
-          email: Faker::Internet.email,
-          password: Faker::Internet.password,
-          role: 'fake_role'
-        }.to_json
-      end
+    let(:user) { create(:user, role:'owner') }
+    let(:headers) { valid_headers.except('Authorization')}
+    let(:valid_credentials) do
+      {
+        email: user.email,
+        password: user.password
+      }.to_json
+    end
+    let(:invalid_credentials) do
+      {
+        email: Faker::Internet.email,
+        password: Faker::Internet.password
+      }.to_json
+    end
 
-      context 'when the request is valid' do
-        before { post '/auth/login', params: valid_credentials, headers: headers}
+    context 'when the request is valid' do
+      before { post '/auth/login', params: valid_credentials, headers: headers}
 
-        it 'returns an authentication token' do
-          expect(json['auth_token']).not_to be_nil
-        end
-      end
-
-      context 'when the request is invalid' do
-        before { post '/auth/login', params: invalid_credentials, headers:headers}
-
-        it 'returns a failure message' do
-          expect(json['message']).to match(/Invalid credentials/)
-        end
+      it 'returns an authentication token' do
+        expect(json['auth_token']).not_to be_nil
       end
     end
-    context 'when role is vet' do
-      let!(:user) { create(:user, role:'vet') }
-      let(:headers) { valid_headers.except('Authorization')}
-      let(:valid_credentials) do
-        {
-          email: user.email,
-          password: user.password,
-          role: user.role
-        }.to_json
-      end
-      let(:invalid_credentials) do
-        {
-          email: Faker::Internet.email,
-          password: Faker::Internet.password,
-          role: 'fake_role'
-        }.to_json
-      end
 
-      context 'when the request is valid' do
-        before { post '/auth/login', params: valid_credentials, headers: headers}
+    context 'when the request is invalid' do
+      before { post '/auth/login', params: invalid_credentials, headers:headers}
 
-        it 'returns an authentication token' do
-          expect(json['auth_token']).not_to be_nil
-        end
-      end
-
-      context 'when the request is invalid' do
-        before { post '/auth/login', params: invalid_credentials, headers:headers}
-
-        it 'returns a failure message' do
-          expect(json['message']).to match(/Invalid credentials/)
-        end
+      it 'returns a failure message' do
+        expect(json['message']).to match(/Invalid credentials/)
       end
     end
   end
