e76ae64f670926cd765fcc864a4b2ab9da5573cf
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-05 17:15:32 +0200

    changed opet#regoister to registration#create

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,20 +1,7 @@
 class PetsController < ApplicationController
   def create
-      pet = current_user.pets.create!(name: pet_params[:name], animal: pet_params[:animal])
-      json_response(current_user, status = :created)
-  end
-
-  def register
-    pet = Pet.find(pet_params[:id])
-    vet = User.find_by(email: pet_params[:vet_email])
-    if vet
-      user = pet.users.where(role: 'owner').last
-
-      pet.registrations.create!(user_id:vet.id, registration_date: DateTime.now, vet_registration: true)
-      json_response(current_user, status = :created)
-    else
-      json_response({message: "Vet does't exist"}, staus = :unprocessable_entity)
-    end
+    pet = current_user.pets.create!(name: pet_params[:name], animal: pet_params[:animal])
+    json_response(current_user, status = :created)
   end
 
   private
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,6 +8,19 @@ class RegistrationsController < ApplicationController
     json_response(registration.user)
   end
 
+  def create
+    # binding.pry
+    pet = Pet.find(params[:pet_id])
+    vet = User.find_by(email: params[:vet_email])
+    if vet.present?
+      user = pet.users.find_by(role: 'owner')
+
+      pet.registrations.create!(user_id:vet.id, registration_date: DateTime.now, vet_registration: true)
+      json_response(current_user, status = :created)
+    else
+      json_response({message: "Vet does't exist"}, staus = :unprocessable_entity)
+    end
+  end
   private
   def registration_params
     params.permit(:id)
