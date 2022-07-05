95b8b3799823bbd0c7e62cece9bd4b2544300af1
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-05 17:23:02 +0200

    refactored use of params

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,11 +1,11 @@
 class PetsController < ApplicationController
   def create
-    pet = current_user.pets.create!(name: pet_params[:name], animal: pet_params[:animal])
+    pet = current_user.pets.create!(pet_params)
     json_response(current_user, status = :created)
   end
 
   private
   def pet_params
-    params.permit(:id, :name, :animal, :vet_email)
+    params.permit(:name, :animal)
   end
 end
