cf71e3bdf6e5f25cf7259a37ab0a1479b0c8fea4
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-06 15:29:32 +0200

    refactored layout

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,7 @@ class RegistrationsController < ApplicationController
   def update
     registration = Registration.find(params[:id])
     registration.update(accepted: true)
-
     user = registration.pet.users.where(role: 'owner').last
-
     json_response(registration.user)
   end
 
@@ -13,7 +11,6 @@ class RegistrationsController < ApplicationController
     vet = User.find_by(email: params[:vet_email])
     if vet.present?
       user = pet.users.find_by(role: 'owner')
-
       pet.registrations.create!(user_id:vet.id, registration_date: DateTime.now, vet_registration: true)
       json_response(current_user, status = :created)
     else
