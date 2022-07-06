365f6987719379534f635fab535a12c7665af14e
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-06 10:03:42 +0200

    removed pry

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,7 +3,6 @@ class AppointmentsController < ApplicationController
     pet = Pet.find(params[:id])
     if (pet.present? && User.find_by(email: params[:vet_email]))
       vet = User.find_by(email: params[:vet_email])
-      binding.pry
       registration = pet.registrations.find_by(user_id: vet.id)
       registration.appointments.create!(date: params[:date].to_datetime)
       json_response(pet.owner, status =  :created)
