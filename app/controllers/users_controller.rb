dcf3efb9acd03638e5ef67368d97564486cd3b6a
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-06 10:21:06 +0200

    refactored layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,9 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: [:create, :authenticate]
+
   def create
     user = User.create!(user_params)
     auth_response(user, status = :created)
-  end
-
-  def show
-    json_response(current_user)
-  end
-
-  def vets
-    json_response(User.where(role: 'vet'))
   end
 
   def authenticate
@@ -23,6 +16,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    json_response(current_user)
+  end
+
+  def vets
+    json_response(User.where(role: 'vet'))
+  end
+  
   private
   def user_params 
     params.permit(:name,:email,:password,:password_confirmation, :role)
