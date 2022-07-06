899c9be5787871477c3851efd8d5f0033c1d4dbb
ReeganCodemaster <reeganviljoen@outlook.com>
2022-07-06 10:24:44 +0200

    refactored layout

--- app/serializers/registration_serializer.rb
+++ app/serializers/registration_serializer.rb
@@ -1,7 +1,9 @@
 class RegistrationSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :pet_id, :registration_date, :accepted, :user, :vet_registration
+
    def user 
     object.user.name
    end
+   
   has_many :appointments
 end
